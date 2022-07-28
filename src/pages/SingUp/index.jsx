d42ff7fc440fd7613051126005044e8866c58d39
pedrocsc99 <pedro.csc.99@gmail.com>
2022-07-28 00:42:28 -0300

    Create user

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,27 +1,63 @@
-import { Container, Form,Background } from "./styles";
-import {FiMail,FiLock,FiUser} from "react-icons/fi"
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Container, Form, Background } from "./styles";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
+import { Link, useNavigate} from "react-router-dom";
+import { api } from "../../service/api";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-import  {Input} from '../../components/Input'
-import  {Button} from '../../components/Button'
+export function SingUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+  function handleSingUp() {
+    if(!name || !email || !password){
+       return alert("Preencha todos os campos!")
+    }
 
-export function SingUp(){
-    return(
-        <Container>
-            <Background/>
-            <Form>
-                <h1>Rocket Notes</h1>
-                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+    api.post("/users", {name,email,password}).then(()=>{
+        alert("Usuario cadastrado com sucesso")
+        navigate("/")
+    }).catch(error =>{
+        if (error.response) {
+           alert(error.response.data.message)
+        }else{
+            alert("Não foi possível cadastrar")
+        }
+    })
+  }
+  
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-                <h2>Crie sua Conta</h2>
-                <Input placeholder="Nome" type="text" icon={FiUser}></Input>
-                <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
-                <Input placeholder="senha" type="password" icon={FiLock}></Input>
+        <h2>Crie sua Conta</h2>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
+        ></Input>
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={e=> setEmail(e.target.value)}
+        ></Input>
+        <Input
+          placeholder="senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        ></Input>
 
-                <Button title='Cadastrar'/>
-                <Link to="/">Voltar para o Login</Link>
-            </Form>
-            
-        </Container>
-    )
+        <Button title="Cadastrar" onClick={handleSingUp} />
+        <Link to="/">Voltar para o Login</Link>
+      </Form>
+    </Container>
+  );
 }
